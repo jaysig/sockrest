d482913f6b283cbc4dd90926ebd4c8a0105396e2
Jaysig <jasontsigmon@gmail.com>
2016-03-09 15:16:59 -0600

    finished starter version

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -3,3 +3,16 @@ Template.postsList.helpers({
 		return Posts.find();
 	}
 });
+
+Template.postsList.rendered = function(){
+	$('.grid').imagesLoaded(function(){
+		// images have loaded
+		// User the Packery jQuery plugin
+		$('.grid').packery({
+			// options
+			itemSelector: '.item',
+			//horizontal and vertical spacing between elements
+			gutter: 10
+		});
+	});
+};
